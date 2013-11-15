7fef3936887168bf004b17daa252af841498ad21
Justin Jeffress <jjeffress>
2013-11-14 17:23:09 -0800

    Initial Commit
    Signed-off-by: Justin Jeffress <jjeffress>

---
+++ ruby_modules/constants.rb
@@ -0,0 +1,18 @@
+# Global constans for Ruby scripts, classes, modules, etc.
+# Written By: Justin Jeffress
+# Version 1.0
+
+#The amazon ecs keys (used for referring to the key values in a config.json file)
+class CONST_AWS
+	LABEL = "amazon-ecs"
+	ASSOCIATE_TAG = "associate_tag"
+	ACCESS_KEY_ID = "access_key_id"
+	SECRET_KEY = "secret_key"
+end
+
+#The parse.com keys (used for referring to the key values in a config.json file)
+class CONST_PARSE
+	LABEL = "parse"
+	APPLICATION_ID = "application_id"
+	API_KEY = "api_key"
+end
