8ba8384b627ac6600899aa60e9cef77cbb6a38fe
Justin Jeffress <jjeffress>
2013-12-18 13:36:46 -0800

    Save Apple Sales Data to parse
    Save the report data extracted from the page via selenium to parse.com
    
    Signed-off-by: Justin Jeffress <jjeffress>

--- salesdata_extraction/apple_reporter.rb
+++ salesdata_extraction/apple_reporter.rb
@@ -1,6 +1,5 @@
 require 'nokogiri'
 require 'parse-ruby-client'
-require 'pp'
 
 basePath = File.absolute_path(File.dirname(__FILE__))
 # linking to custom modules
@@ -43,10 +42,58 @@ results = Selenium_harness.run(class_name, lambda { | log |
 	the_sales_table.each do | row |
 		row_hash = Hash.new
 		row_hash[:title] = row.children[0].text
-		row_hash[:units] = row.children[4].text
+		row_hash[:units_sold] = row.children[4].text
 		row_hash[:country] = row.children[7].text
 		row_hash[:apple_id] = row.children[8].text
 		results.push(row_hash)
 	end
 	return results
 })
+
+
+def get_book_hash()
+	book_hash = Hash.new
+
+	#getting the number of books in parse
+	book_count = Parse::Query.new("Book").tap do |q|
+   	q.exists("appleId")
+   	q.limit = 0
+   	q.count = 1
+	end.get	 
+
+	#requesting all books at once
+	#TODO: parse is limited to 1000 rows per query. Need to update this to loop requests 
+	#using skip to get everything.
+	book_list = Parse::Query.new("Book").tap do |q|
+		q.exists("appleId")
+		q.limit = book_count["count"]
+	end.get
+
+	book_list.each do | book |
+		book_hash[book["appleId"]] = book
+	end
+	return book_hash
+end
+
+def save_sales_data_to_parse(results)
+	book_hash = get_book_hash()
+	results.each do | result |
+		apple_sales_data = Parse::Object.new("AppleSalesData")
+		apple_sales_data["book"] = book_hash[result[:apple_id].to_i]
+		apple_sales_data["appleId"] = result[:apple_id].to_i
+		apple_sales_data["dailySales"] = result[:units_sold].to_i
+		apple_sales_data["country"] = result[:country]
+		#TODO: get the date out of the page.
+		apple_sales_data["crawlDate"] = Parse::Date.new((Date.today).strftime("%Y/%m/%d")+" 00:00:00")
+		
+		apple_sales_data.save
+		puts "#{result[:title]}\t#{result[:units_sold]}\t#{result[:country]}\t#{result[:apple_id]}"
+	end
+end
+
+if !results.nil? && results.count > 0
+	#initialize parse
+	Parse.init :application_id => BT_CONSTANTS[:parse_application_id],
+		        :api_key        => BT_CONSTANTS[:parse_api_key]
+	save_sales_data_to_parse(results)
+end
