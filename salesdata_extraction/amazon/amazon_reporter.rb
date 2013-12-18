fa42dc0770982c7a9baace2b18564b2de87807aa
Justin Jeffress <jjeffress>
2013-12-17 19:24:51 -0800

    Save extracted data to Parse.com
    Updated to save the data extracted from selenium into Parse.com
    
    Signed-off-by: Justin Jeffress <jjeffress>

--- salesdata_extraction/amazon/amazon_reporter.rb
+++ salesdata_extraction/amazon/amazon_reporter.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'parse-ruby-client'
 
 basePath = File.absolute_path(File.dirname(__FILE__))
 # linking to custom modules
@@ -6,7 +7,7 @@ require File.join(basePath, "..", "..", "ruby_modules", "constants")
 require File.join(basePath, "..", "..", "ruby_modules", "selenium_harness")
 
 class_name = "Salesdata_Extraction::Amazon_reporter"
-results = Selenium_harness.run(class_name, lambda {
+results = Selenium_harness.run(class_name, lambda { | log |
 	BT_CONSTANTS = BTConstants.get_constants
 	url = BT_CONSTANTS[:amazon_kdp_url]
 	
@@ -53,7 +54,7 @@ results = Selenium_harness.run(class_name, lambda {
 	}
 	
 	results.concat(print_lambda.call(Array.new))
-			
+
 	report_select = Selenium_harness.find_element(:id, "marketplaceSelect")
 	report_options = report_select.find_elements(:tag_name, "option")
 	report_options.each do | option |
@@ -68,6 +69,89 @@ results = Selenium_harness.run(class_name, lambda {
 	end
 	return results
 })
-results.each do | result |
-puts "#{result[:title]}\t#{result[:asin]}\t#{result[:net_sales]}\t#{result[:force_free]}\t#{result[:country]}"
+
+def get_book_hash()
+
+	book_hash = Hash.new
+	
+	#getting the number of books in parse
+	book_count = Parse::Query.new("Book").tap do |q|
+   	q.limit = 0
+   	q.count = 1
+	end.get	 
+
+	#requesting all books at once
+	#TODO: parse is limited to 1000 rows per query. Need to update this to loop requests 
+	#using skip to get everything.
+	book_list = Parse::Query.new("Book").tap do |q|
+		q.limit = book_count["count"]
+	end.get
+
+	#building the book_hash
+	book_list.each do | book |
+		book_hash[book["asin"]] = book
+	end
+	
+	return book_hash
+end
+
+def save_sales_data_to_parse(results)
+
+	book_hash = get_book_hash
+
+	results.each do | result |
+		
+		net_sales = result[:net_sales].to_i
+		daily_sales = net_sales
+		
+		#setting the crawl date
+		crawl_date = Parse::Date.new((Date.today).strftime("%Y/%m/%d")+" "+Time.now().strftime("%H:%M:%S"))
+	
+		#TODO: only do this if it's not the first of the month.
+		#checking to see if we have a record from the previous day. 
+		old_sales_data = Parse::Query.new("AmazonSalesData").tap do |q|
+			q.greater_than("crawlDate", Parse::Date.new(((Date.today-1).strftime("%Y/%m/%d")+" "+"00:00:00")))
+			q.eq("asin", result[:asin])
+			q.eq("country", result[:country])
+			q.order_by = "crawlDate"
+			q.order = :descending
+			q.limit = 1
+		end.get.first
+	
+		#amazon tracks month to date sales, so we need to subtract yesterday's net from today's
+		if !old_sales_data.nil?
+			daily_sales = net_sales - old_sales_data["netSales"].to_i
+			puts "daily sales: #{daily_sales}"
+		end
+	
+		#getting the book object to link the amazon_sales_data to.
+		book = book_hash[result[:asin]]
+		
+		#if we lack a book then it's not in parse so we add it. Next time the amazon book pick up tool runs
+		#the rest of the data will be filled out.
+		if book.nil?
+			book = Parse::Object.new("Book")
+			book["asin"] = result[:asin]
+			book.save
+		end
+	
+		amazon_sales_data = Parse::Object.new("AmazonSalesData")
+		amazon_sales_data["book"] = book
+		amazon_sales_data["asin"] = result[:asin]
+		amazon_sales_data["netSales"] = net_sales
+		amazon_sales_data["forceFree"] = result[:force_free].to_i
+		amazon_sales_data["country"] = result[:country]
+		amazon_sales_data["crawlDate"] = crawl_date
+		amazon_sales_data["dailySales"] = daily_sales
+		
+		amazon_sales_data.save
+		
+	end
+end
+
+if !results.nil? && results.count > 0
+	#initialize parse
+	Parse.init :application_id => BT_CONSTANTS[:parse_application_id],
+		        :api_key        => BT_CONSTANTS[:parse_api_key]
+	save_sales_data_to_parse(results)
 end
