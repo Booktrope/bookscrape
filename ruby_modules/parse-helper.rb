c92de4cc7bb5b95ca7f32d9878f7ac1aff1e7ea2
Justin Jeffress <justin.jeffress@booktrope.com>
2014-07-25 18:38:06 -0700

    Google Play/ParseHelper
    Updated google_play analysis tool to confirm price changes.
    
    Updated ParseHelper with a helper method to load the PriceChangeQueue from parse.com
    
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

--- ruby_modules/parse-helper.rb
+++ ruby_modules/parse-helper.rb
@@ -33,5 +33,62 @@ module Booktrope
 			return books
 		end
 		
+		def ParseHelper.get_price_change_queue(options = [])
+			change_queue = Parse::Query.new("PriceChangeQueue").tap do | q |
+				options.each do | option |
+					ParseHelper.add_constraint option, q
+				end
+			end.get	
+		end
+		
+		private
+		#VALID_OPTIONS = [ :less_than, :less_eq, :greater_than, :greater_eq, :not_eq, :eq, :value_in, :exists ]
+		def ParseHelper.add_constraint(constraint, query)
+			case constraint.option
+			when :less_than
+				query.less_than(constraint.field_name, constraint.value)
+			when :less_eq
+				query.less_eq(constraint.field_name, constraint.value)
+			when :greater_than
+				query.greater_than(constraint.field_name, constraint.value)
+			when :greater_eq
+				query.greater_eq(constraint.field_name, constraint.value)
+			when :not_eq
+				query.not_eq(constraint.field_name, constraint.value)
+			when :eq
+				query.eq(constraint.field_name, constraint.value)
+			when :value_in
+				query.value_in(constraint.field_name, constraint.value)
+			when :exists
+				query.eq(constraint.field_name, constraint.value)
+			when :order_by
+				query.order_by = constraint.value
+			when :order
+				query.order = constraint.value
+			when :limit
+				query.limit = constraint.value
+			when :skip
+				query.skip = constraint.value
+			when :include
+				query.include = constraint.value
+			else
+				#TODO:: warn about unsupported constraint
+			end
+		end
+		
+	end
+	
+	class Constraint
+		attr_accessor :option, :field_name, :value
+		
+		def initialize(option, field_name, value)
+			@option = option
+			@field_name = field_name
+			@value = value
+		end
+		
+		def to_s
+			"Option: #{@option} \nField Name: #{@field_name}\nValue: #{@value}\n"
+		end
 	end
 end
