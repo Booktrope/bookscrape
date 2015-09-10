566bc1c830569b50cd791a303b028d4f40b3a9f1
Justin <justin.jeffress@booktrope.com>
2015-09-09 18:40:24 -0700

    Convert tabs to spaces
    Signed-off-by: Justin <justin.jeffress@booktrope.com>

--- ruby_modules/parse-ruby-client-monkey.rb
+++ ruby_modules/parse-ruby-client-monkey.rb
@@ -1,83 +1,84 @@
+gem 'parse-ruby-client', '=0.2.0'
 require 'parse-ruby-client'
 
 module Parse
-	class Object
-		def save_perserve(fields = [])
-			perserve_these = Array.new
-			fields.each do | field |
-				perserve_these.push self[field]
-			end
-			
-			self.save
-			
-			fields.each_with_index do | field, index |
-				self[field] = perserve_these[index]
-			end
-		end
-	end
-	class Batch
-		attr_accessor :max_requests
+  class Object
+    def save_perserve(fields = [])
+      perserve_these = Array.new
+      fields.each do | field |
+        perserve_these.push self[field]
+      end
 
-		def add_request_run_when_full(request)
-			if @requests.length >= @max_requests
-				self.run!
-				@requests.clear
-			end
-			@requests << request
-		end
+      self.save
 
-		def create_object_run_when_full!(object)
-			method = "POST"
-			path = Parse::Protocol.class_uri(object.class_name)
-			body = object.safe_hash
-			add_request_run_when_full({
-				"method" => method,
-				"path" => path,
-				"body" => body
-			})
-		end		
+      fields.each_with_index do | field, index |
+        self[field] = perserve_these[index]
+      end
+    end
+  end
+  class Batch
+    attr_accessor :max_requests
 
-		def update_object_run_when_full!(object)
-			method = "PUT"
-			path = Parse::Protocol.class_uri(object.class_name, object.id)
-			body = object.safe_hash
-			add_request_run_when_full({
-				"method" => method,
-				"path" => path,
-				"body" => body
-			})
-		end
-		
-		def delete_object_run_when_full!(object)
-			add_request_run_when_full({
-				"method" => "DELETE",
-				"path" => Parse::Protocol.class_uri(object.class_name, object.id)
-			})
-		end
-		
-		def marshal_dump
-			[requests, client]
-		end
-		
-		def marshal_load(array)
-			@requests, client = array
-		end
-		
-	end
-	
-	class Date
-		def marshal_load(array)
-			
-		end
-	end
-	
-	class Client
-		def marshal_dump
-			[host, application_id, api_key, master_key, session_token, max_retries]
-		end
-		
-		def marshal_load(array)
-			host, application_id, api_key, master_key, session_token, max_retries = array
-		end
-	end
+    def add_request_run_when_full(request)
+      if @requests.length >= @max_requests
+        self.run!
+        @requests.clear
+      end
+      @requests << request
+    end
+
+    def create_object_run_when_full!(object)
+      method = "POST"
+      path = Parse::Protocol.class_uri(object.class_name)
+      body = object.safe_hash
+      add_request_run_when_full({
+        "method" => method,
+        "path" => path,
+        "body" => body
+      })
+    end
+
+    def update_object_run_when_full!(object)
+      method = "PUT"
+      path = Parse::Protocol.class_uri(object.class_name, object.id)
+      body = object.safe_hash
+      add_request_run_when_full({
+        "method" => method,
+        "path" => path,
+        "body" => body
+      })
+    end
+
+    def delete_object_run_when_full!(object)
+      add_request_run_when_full({
+        "method" => "DELETE",
+        "path" => Parse::Protocol.class_uri(object.class_name, object.id)
+      })
+    end
+
+    def marshal_dump
+      [requests, client]
+    end
+
+    def marshal_load(array)
+      @requests, client = array
+    end
+
+  end
+
+  class Date
+    def marshal_load(array)
+
+    end
+  end
+
+  class Client
+    def marshal_dump
+      [host, application_id, api_key, master_key, session_token, max_retries]
+    end
+
+    def marshal_load(array)
+      host, application_id, api_key, master_key, session_token, max_retries = array
+    end
+  end
 end
