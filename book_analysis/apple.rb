e324c9c29f8b6fffd30cb6c268882a38f7f150c3
Justin <justin.jeffress@booktrope.com>
2015-09-18 13:29:57 -0700

    Apple - prevent crash if control number has space
    Signed-off-by: Justin <justin.jeffress@booktrope.com>

--- book_analysis/apple.rb
+++ book_analysis/apple.rb
@@ -22,11 +22,11 @@ Extracts various meta data from iBooks using the iTunes search api.
    Usage:
             ruby apple.rb [--dontSaveToParse] --testRJMetrics --dontSaveToParse --dontSaveToRJMetrics
    EOS
-   
+
    opt :testRJMetrics, "Use RJMetrics test sandbox. This option will save to the sandbox.", :short => 't'
    opt :dontSaveToParse, "Prevents the collected data from being saved to parse.", :short => 'x'
    opt :dontSaveToRJMetrics, "Turns of RJMetrics entirely. Data wont be saved to either the sandbox or live.", :short => 'r'
-   
+
    version "2.0.0 2014 Justin Jeffress"
 
 end
@@ -38,7 +38,7 @@ is_test_rj = ($opts.testRJMetrics) ? true : false
 $rjClient = Booktrope::RJHelper.new Booktrope::RJHelper::APPLE_STATS_TABLE, ["parse_book_id", "crawlDate"], is_test_rj if !$opts.dontSaveToRJMetrics
 
 Booktrope::ParseHelper.init_production
-	        
+
 $batch = Parse::Batch.new
 $batch.max_requests = 50
 
@@ -77,7 +77,7 @@ end.get
 unconfirmed_hash = Hash.new
 change_queue = Parse::Query.new("PriceChangeQueue").tap do |q|
 	q.limit = 1000
-	q.eq("status", Booktrope::PRICE_CHANGE::UNCONFIRMED) 
+	q.eq("status", Booktrope::PRICE_CHANGE::UNCONFIRMED)
 	q.in_query("salesChannel", Parse::Query.new("SalesChannel").tap do | inner_query |
 		inner_query.eq("name", Booktrope::PRICE_CHANGE::APPLE_CHANNEL)
 	end)
@@ -119,16 +119,16 @@ book_hash.each do | key, book_container |
 		book_container[:status] = BOOK_ANALYSIS_LOOKUP_STATUS_SKIPPED
 		next
 	end
-	
-	control_number = book[book_container[:control_number]]	
+
+	control_number = book[book_container[:control_number]]
 	lookup = "#{(book_container[:control_number] == "appleId")? "id": "isbn" }=#{control_number}"
-	
+
 	case book_container[:control_number]
-	when "appleId" 
-		id_str <<  "#{id_str.length > 0 ?",":""}#{control_number}"
+	when "appleId"
+		id_str <<  "#{id_str.length > 0 ?",":""}#{control_number.strip}"
 		count = count + 1
 	when "epubIsbnItunes"
-		isbn_str << "#{isbn_str.length > 0 ?",":""}#{control_number}"
+		isbn_str << "#{isbn_str.length > 0 ?",":""}#{control_number.strip}"
 		count = count + 1
 	else
 		puts "NONE"
@@ -136,7 +136,7 @@ book_hash.each do | key, book_container |
 
 	if count % max == 0
 		request_url = "#{$BT_CONSTANTS[:itunes_lookup_url]}?id=#{id_str}&isbn=#{isbn_str}"
-		request_urls.push request_url		
+		request_urls.push request_url
 		id_str = ""
 		isbn_str = ""
 	end
@@ -150,18 +150,18 @@ isbn_str = ""
 request_urls.each do | request_url |
 	response = Download_simple.downloadData(request_url)
 	if response.code == "200"
-	
+
       json = JSON.parse(response.body)
 
       if json["resultCount"] == 0
          log.warn "No Results: #{request_url}"
          next
       end
-      
+
       results = json["results"]
-   
+
       id, author, apple_id, title, price, detailUrl, averageUserRating, userRatingCount, imageUrl100 = ""
-   
+
       results.each do |result|
       	apple_id = result["trackId"].to_s
          author = result["artistName"]
@@ -171,44 +171,44 @@ request_urls.each do | request_url |
          averageUserRating = result["averageUserRating"]
          userRatingCount =  result["userRatingCount"]
          imageUrl100 = result["artworkUrl100"]
-         
+
          puts "\"%s\",%s,%s,%s,%s,%s,%s,%s" % [title, author, price, apple_id, userRatingCount, averageUserRating, detailUrl, imageUrl100] if $opts.dontSaveToParse
-         
+
          if book_hash.has_key? apple_id
          	book = book_hash[apple_id][:book]
 	         book_hash[apple_id][:status] = BOOK_ANALYSIS_LOOKUP_STATUS_FOUND
-	         
+
 	         if unconfirmed_hash.has_key? book
 	         	log.info "found a book with a price change. #{book["title"]} appleId: #{apple_id} #{unconfirmed_hash[book].id} #{price} #{unconfirmed_hash[book]["price"]}"
 	         	#Confirming that price_changer.rb has properly changed the price of the book.
 	         	if unconfirmed_hash[book]["price"] == price
 	         		log.info "CONFIRMED: Expected: #{unconfirmed_hash[book]["price"]} Actual: #{price}"
 		         	unconfirmed_hash[book]["status"] = Booktrope::PRICE_CHANGE::CONFIRMED
-		         	#Don't batch these up since we dont do these often and status is time 
+		         	#Don't batch these up since we dont do these often and status is time
 	   	      	#sensitive so we want to know as soon as it's confirmed.
-	      	   	unconfirmed_hash[book].save #if !$opts.dontSaveToParse 
+	      	   	unconfirmed_hash[book].save #if !$opts.dontSaveToParse
 	         		sleep(1.0)
 	         	end
 	         end
-	         
+
 	   		crawl_date = Parse::Date.new(Time.now.utc.strftime("%Y/%m/%d %H:%M:%S"))
 				book_is_dirty = false
 	         #updating our book with its appleId, if we found the book via epubIsbnItunes
-	         
+
 	         if book["detailUrlApple"] != detailUrl
 	         	book["detailUrlApple"] = detailUrl
 	         	book_is_dirty = true
 	         end
-	         
+
 	         if book["largeImageApple"] != imageUrl100
 	         	book["largeImageApple"] = imageUrl100
 	         	book_is_dirty = true
 	         end
-	         
+
 	         if book_is_dirty && !$opts.dontSaveToParse
 	         	!$batch.update_object_run_when_full!(book)
 	         end
-	         
+
 	         appleStats = Parse::Object.new("AppleStats")
 	         appleStats['book'] = book
 	         appleStats['appleId'] = apple_id.to_i
@@ -238,7 +238,7 @@ end.get
 
 not_found_hash = Hash.new
 not_found.each do | book |
-	not_found_hash[book["book"]["objectId"]] = book if !book["book"].nil? 
+	not_found_hash[book["book"]["objectId"]] = book if !book["book"].nil?
 end
 
 apple_channel = Parse::Query.new("SalesChannel").tap do | q |
@@ -254,7 +254,7 @@ book_hash.sort_by{|k| k[1][:book]["title"]}.each do | key, book_meta |
 			not_found_book["salesChannel"] = apple_channel
 			not_found_book["reasonCode"] = book_meta[:status]
 			$batch.create_object_run_when_full!(not_found_book) if !$opts.dontSaveToParse
-		end		
+		end
 	end
 end
 
